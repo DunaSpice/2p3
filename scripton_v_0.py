be11557d7db1dc18b60894b6f9755b78e9123417
Pavlo Safronov <SafronovPN@gmail.com>
2024-02-05 16:08:55 +0800

    move function in config.json

--- scripton_v_0.py
+++ scripton_v_0.py
@@ -1,31 +1,25 @@
 import json
 import os
-import subprocess
+import platform
 from dotenv import load_dotenv
 from openai import OpenAI
 
+# Load configuration from JSON file
+config = json.load(open('config.json'))
+exec(config["tool_import"])
+exec(config["tool_function"])
 # Load .env file
 load_dotenv()
 # Load OpenAI API key from environment variable
 api_key = os.getenv("OPENAI_API_KEY")
-# Load configuration from JSON file
-config = json.load(open('config.json'))
-
-
-# Define the execute_command function
-def execute_command(command):
-    try:
-        result = subprocess.run(command, shell=True, capture_output=True, text=True, check=True).stdout.strip()
-        return {"success": True, "output": result or "done"}
-    except Exception as e:
-        return {"success": False, "error": str(e)}
 
 
 # Define the function 'main' which is the entry point of the script
 def main():
     client = OpenAI(api_key=api_key)
-    # To store the conversation history
-    conversation = [{"role": "system", "content": f"Your operation system is {os.name}. " + config["system_message"]}]
+    # Conversation history
+    conversation = [{"role": "system", "content": f"You are assistant only in operating system {platform.system()}, "
+                                                  f"it important. {config['system_message']}"}]
 
     while True:
         # User for input
@@ -33,7 +27,6 @@ def main():
         # Break out of the prompt loop to stop the program
         if user_content.lower() == config["exit_command"]:
             break
-
         # Append user message to conversation history
         conversation.append({"role": "user", "content": user_content})
 
@@ -42,45 +35,43 @@ def main():
             response = client.chat.completions.create(
                 model=config["openai_model"],
                 messages=conversation,
-                tools=[config["tool"]],
+                tools=[config["tool_arg"]],
                 tool_choice="auto",
             )
             if config.get("debug", False):
                 print(f"Debug: To AI messages = {conversation}\nDebug: AI RawResponse = {response}")
 
-            if response.choices[0].message.content:
-                print(response.choices[0].message.content)
-                conversation.append({"role": "assistant", "content": response.choices[0].message.content})
+            res_message = response.choices[0].message
+
+            if res_message.content:
+                print(res_message.content)
+                conversation.append({"role": "assistant", "content": res_message.content})
 
             # Check if 'tool_calls' attribute exists and is not None
-            if hasattr(response.choices[0].message, 'tool_calls') and response.choices[
-                0].message.tool_calls is not None:
-                for tool_call in response.choices[0].message.tool_calls:
+            if hasattr(res_message, 'tool_calls') and res_message.tool_calls is not None:
+                for tool_call in res_message.tool_calls:
                     if getattr(tool_call, 'type', None) == 'function':
                         function_name = getattr(tool_call.function, 'name', None)
                         arguments = getattr(tool_call.function, 'arguments', "{}")
 
-                        if function_name == 'execute_command':
+                        if function_name in globals():
                             # Ensure that arguments are properly formatted as a string
-                            command = json.loads(arguments).get('command', '')
-                            if isinstance(command, str):
+                            arg = json.loads(arguments).get('command', '')
+                            if isinstance(arg, str):
                                 # Execute the command and print the result
-                                result = execute_command(command)
-                                result_message = f"Command execution result: {result['output']}" if result[
-                                    'success'] else f"Error: {result['error']}"
+                                result_func = f"Function result: {globals()[function_name](arg)}"
                                 # Send the tool result back to the model for further conversation
-                                conversation.append({"role": "assistant", "name": "tool", "content": result_message})
-                                # TODO conversation.append({"role": "tool", "content": result_message, "tool_call_id": tool_id})
-                                print(result_message)
+                                conversation.append({"role": "user", "content": result_func})
+                                # TODO tool answer now not work conversation.append({"role": "tool", "content": result_func, "tool_call_id": tool_id})
+                                print(result_func)
                             else:
-                                conversation.append({"role": "assistant", "name": "tool",
-                                                     "content": f"Invalid command format.={command}"})
-                                print(f"Invalid command format.={command}")
+                                conversation.append({"role": "user", "content": f"Invalid arg={arg}"})
+                                print(f"Invalid arg format.={arg}")
                         else:
                             raise Exception(f"function {function_name} not exist")
         except Exception as e:
-            conversation.append({"role": "user", "name": "Exception", "content": e})
-            print(f"Invalid command format.={e}")
+            conversation.append({"role": "user", "name": "Exception", "content": f"{e}"})
+            print(f"Exception: {e}")
 
 
 # Check if the script is the main module being executed, and if so, call main()
